524a6b53358ac8bb94788c4bc06bb2de4b22f298
Resi Respati <resir014@gmail.com>
2018-04-24 23:06:20 +0700

    add logging to dummy controller

--- components/controllers/dummy_controller.ts
+++ components/controllers/dummy_controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-import { Component } from 'merapi';
+import { Component, ILogger } from 'merapi';
 
 export default class DummyController extends Component {
-  constructor() {
+  constructor(private logger: ILogger) {
     super();
   }
 
   public async ok(req: Request, res: Response, next: NextFunction): Promise<void> {
+    this.logger.info('GET /info');
     res.status(200).send({ status: 'ok' });
   }
 }
