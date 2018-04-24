016964b41a89eea89eba2565e81ceac61e2e4219
Resi Respati <resir014@gmail.com>
2018-04-24 22:52:49 +0700

    some minor fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 // Load our `service.yml` file which contains our service metadata.
 const config = yaml.safeLoad(fs.readFileSync('./service.yml', 'utf8'));
-config.package = require('./package');
+config.package = require('./package.json');
 
 module.exports = merapi({
   basepath: path.resolve(__dirname, 'lib'),
